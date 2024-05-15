b0424d5cffc0dd266e6dad02e3b01da486f4d4d4
fred2020nic <60634761+fred2020nic@users.noreply.github.com>
2024-05-15 14:51:11 -0600

    first commit

---
+++ initialize.php
@@ -0,0 +1,30 @@
+<?php
+
+$host = $_SERVER['HTTP_HOST'];
+//  var_dump($host);
+
+ if ($host === 'localhost' ) {
+
+
+$dev_data = array('id' => '-1', 'firstname' => 'Mauricio', 'lastname' => 'Sevilla', 'username' => 'configuroweb', 'password' => '4b67deeb9aba04a5b54632ad19934f26', 'last_login' => '', 'date_updated' => '', 'date_added' => '');
+if (!defined('base_url')) define('base_url', 'http://localhost/0.1_cw/4.e8/e8_cofrades/');
+if (!defined('base_app')) define('base_app', str_replace('\\', '/', __DIR__) . '/');
+if (!defined('dev_data')) define('dev_data', $dev_data);
+if (!defined('DB_SERVER')) define('DB_SERVER', "localhost");
+if (!defined('DB_USERNAME')) define('DB_USERNAME', "root");
+if (!defined('DB_PASSWORD')) define('DB_PASSWORD', "");
+if (!defined('DB_NAME')) define('DB_NAME', "cofrades");
+
+}else{
+
+
+    $dev_data = array('id' => '-1', 'firstname' => 'Mauricio', 'lastname' => 'Sevilla', 'username' => 'configuroweb', 'password' => '4b67deeb9aba04a5b54632ad19934f26', 'last_login' => '', 'date_updated' => '', 'date_added' => '');
+    if (!defined('base_url')) define('base_url', 'https://feppp.com/e8_cofrades/');
+    if (!defined('base_app')) define('base_app', str_replace('\\', '/', __DIR__) . '/');
+    if (!defined('dev_data')) define('dev_data', $dev_data);
+    if (!defined('DB_SERVER')) define('DB_SERVER', "localhost");
+    if (!defined('DB_USERNAME')) define('DB_USERNAME', "u363832898_e8_cofrades");
+    if (!defined('DB_PASSWORD')) define('DB_PASSWORD', "#1234Abcd..#");
+    if (!defined('DB_NAME')) define('DB_NAME', "u363832898_e8_cofrades");
+
+}
